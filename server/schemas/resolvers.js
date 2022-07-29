2198010ce2248675982e8ec009e1a840bf787c74
Abstrack5 <armon_ah05@outlook.com>
2022-07-29 03:46:52 -0400

    finished up typeDefs, working on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,8 @@
+const { User, Thought } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+
 const resolvers = {
     Query: {
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,22 +1,47 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String!
-        email: String!
-        bookCount: Int
-        savedBooks: [bookSchema]
-    }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-    type Book {
-        bookId: ID!
-        authors: [String]
-        description: String
-        title: String!
-        image: String
-        link: String
-    }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
 
+  input saveBookInput {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login: (email: String!, password: String!): Auth
+    addUser:(username: String!, email: String!, password: String!): Auth
+    saveBook:(input: saveBookInput): User
+    removeBook(bookId: ID!): User
+  }
 `;
+
+module.exports = typeDefs;
